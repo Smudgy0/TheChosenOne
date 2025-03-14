e53863d943aef4762923f642aee4480941039834
Smudgy0 <theohammond2006@gmail.com>
2025-03-14 16:23:30 +0000

    Changes to the Shop and added basic spirtewoork

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -34,31 +34,36 @@ public class UIManager : MonoBehaviour
     public void ShopClose()
     {
         shopUI.SetActive(false);
+        shopErrorMessageText.text = " ";
     }
 
     public void IncreaseHP()
     {
         if(PM.MaxHP < 6)
         {
-            if(PM.PlayerScore >= 1)
+            if(PM.PlayerScore >= 10)
             {
                 PM.MaxHP = PM.MaxHP + 1;
                 PM.HP = PM.HP + 1;
-                PM.PlayerScore = PM.PlayerScore - 1;
-                shopErrorMessageText.text = "Thank You For Your Purchase";
+                PM.PlayerScore = PM.PlayerScore - 10;
+                shopErrorMessageText.text = "Thank You For Your Purchase.";
             }
             else
             {
-                shopErrorMessageText.text = "You Cannot Affort This";
+                shopErrorMessageText.text = "You Cannot Affort This.";
             }
             if(PM.HP >= 6)
             {
-                HPButton.SetActive(false);
+                shopErrorMessageText.text = "We Cannot Increase Your HP From Now On.";
             }
+        }
+        else if (PM.HP == 6)
+        {
+            shopErrorMessageText.text = "We Cannot Increase Your HP Any Further.";
         }
         else
         {
-            shopErrorMessageText.text = "This Shop Is Out Of Stock";
+            shopErrorMessageText.text = "This Shop Cannot Process Your Purchase.";
         }
     }
 }
